445dbe56dcd84a58770bb5751968f5624bc32166
Alex Guo <458467864@qq.com>
2025-09-25 19:33:19 -0400

    Converted gopro_to_rosbag node to ROS2 version.

--- gopro_to_rosbag.cpp
+++ gopro_to_rosbag.cpp
@@ -2,11 +2,12 @@
 // Created by bjoshi on 10/29/20.
 //
 
-#include <ros/ros.h>
-#include <rosbag/bag.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/MagneticField.h>
-#include <std_msgs/Header.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rosbag2_cpp/writer.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/magnetic_field.hpp>
+#include <std_msgs/msg/header.hpp>
 
 #include <experimental/filesystem>
 #include <iostream>
@@ -18,42 +19,55 @@
 using namespace std;
 namespace fs = std::experimental::filesystem;
 
+
 int main(int argc, char* argv[]) {
-  ros::init(argc, argv, "gopro_to_rosbag");
-  ros::NodeHandle nh_private("~");
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("gopro_to_rosbag");
 
-  string gopro_video;
-  string gopro_folder;
-  string rosbag;
+  node->declare_parameter<std::string>("gopro_video", "");
+  node->declare_parameter<std::string>("gopro_folder", "");
+  node->declare_parameter<std::string>("rosbag", "");
+  node->declare_parameter<double>("scale", 1.0);
+  node->declare_parameter<bool>("compressed_image_format", false);
+  node->declare_parameter<bool>("grayscale", false);
+  node->declare_parameter<bool>("display_images", false);
+  node->declare_parameter<bool>("multiple_files", false);
 
-  bool is_gopro_video = nh_private.getParam("gopro_video", gopro_video);
-  bool is_gopro_folder = nh_private.getParam("gopro_folder", gopro_folder);
+  std::string gopro_video;
+  std::string gopro_folder;
+  std::string rosbag;
+  double scaling;
+  bool compress_images;
+  bool grayscale;
+  bool display_images;
+  bool multiple_files;
+
+  node->get_parameter("gopro_video", gopro_video);
+  node->get_parameter("gopro_folder", gopro_folder);
+  node->get_parameter("rosbag", rosbag);
+  node->get_parameter("scale", scaling);
+  node->get_parameter("compressed_image_format", compress_images);
+  node->get_parameter("grayscale", grayscale);
+  node->get_parameter("display_images", display_images);
+  node->get_parameter("multiple_files", multiple_files);
+
+  bool is_gopro_video = !gopro_video.empty();
+  bool is_gopro_folder = !gopro_folder.empty();
 
   if (!is_gopro_video && !is_gopro_folder) {
-    ROS_FATAL("Please specify the gopro video or folder");
-    ros::shutdown();
+    RCLCPP_FATAL(node->get_logger(), "Please specify the gopro video or folder");
+    rclcpp::shutdown();
+    return 1;
   }
 
-  ROS_FATAL_STREAM_COND(!nh_private.getParam("rosbag", rosbag), "No rosbag file specified");
+  if (rosbag.empty()) {
+    RCLCPP_FATAL(node->get_logger(), "No rosbag file specified");
+    rclcpp::shutdown();
+    return 1;
+  }
 
-  double scaling = 1.0;
-  if (nh_private.hasParam("scale")) nh_private.getParam("scale", scaling);
-
-  bool compress_images = false;
-  if (nh_private.hasParam("compressed_image_format"))
-    nh_private.getParam("compressed_image_format", compress_images);
-
-  bool grayscale = false;
-  if (nh_private.hasParam("grayscale")) nh_private.getParam("grayscale", grayscale);
-
-  bool display_images = false;
-  if (nh_private.hasParam("display_images")) nh_private.getParam("display_images", display_images);
-
-  bool multiple_files = false;
-  if (nh_private.hasParam("multiple_files")) nh_private.getParam("multiple_files", multiple_files);
-
-  rosbag::Bag bag;
-  bag.open(rosbag, rosbag::bagmode::Write);
+  rosbag2_cpp::Writer bag;
+  bag.open(rosbag);
 
   vector<fs::path> video_files;
 
@@ -72,6 +86,7 @@ int main(int argc, char* argv[]) {
   });
   video_files.erase(end, video_files.end());
 
+
   vector<uint64_t> start_stamps;
   vector<uint32_t> samples;
   std::deque<AcclMeasurement> accl_queue;
@@ -81,10 +96,12 @@ int main(int argc, char* argv[]) {
   vector<uint64_t> image_stamps;
 
   bool has_magnetic_field_readings = false;
+
+  // Read from each video chunks and write video to rosbag
   for (uint32_t i = 0; i < video_files.size(); i++) {
     image_stamps.clear();
 
-    ROS_WARN_STREAM("Opening Video File: " << video_files[i].filename().string());
+    RCLCPP_WARN(node->get_logger(), "Opening Video File: %s", video_files[i].filename().string().c_str());
 
     fs::path file = video_files[i];
     GoProImuExtractor imu_extractor(file.string());
@@ -95,26 +112,22 @@ int main(int argc, char* argv[]) {
     }
 
     imu_extractor.getPayloadStamps(STR2FOURCC("ACCL"), start_stamps, samples);
-    ROS_INFO_STREAM("[ACCL] Payloads: " << start_stamps.size()
-                                        << " Start stamp: " << start_stamps[0]
-                                        << " End stamp: " << start_stamps[samples.size() - 1]
-                                        << " Total Samples: " << samples.at(samples.size() - 1));
+    RCLCPP_INFO(node->get_logger(), 
+                "[ACCL] Payloads: %zu Start stamp: %lu End stamp: %lu Total Samples: %u", 
+                start_stamps.size(), start_stamps[0], start_stamps[samples.size() - 1], samples.at(samples.size() - 1));
     imu_extractor.getPayloadStamps(STR2FOURCC("GYRO"), start_stamps, samples);
-    ROS_INFO_STREAM("[GYRO] Payloads: " << start_stamps.size()
-                                        << " Start stamp: " << start_stamps[0]
-                                        << " End stamp: " << start_stamps[samples.size() - 1]
-                                        << " Total Samples: " << samples.at(samples.size() - 1));
+    RCLCPP_INFO(node->get_logger(), 
+                "[GYRO] Payloads: %zu Start stamp: %lu End stamp: %lu Total Samples: %u", 
+                start_stamps.size(), start_stamps[0], start_stamps[samples.size() - 1], samples.at(samples.size() - 1));
     imu_extractor.getPayloadStamps(STR2FOURCC("CORI"), start_stamps, samples);
-    ROS_INFO_STREAM("[Image] Payloads: " << start_stamps.size()
-                                         << " Start stamp: " << start_stamps[0]
-                                         << " End stamp: " << start_stamps[samples.size() - 1]
-                                         << " Total Samples: " << samples.at(samples.size() - 1));
+    RCLCPP_INFO(node->get_logger(), 
+                "[Image] Payloads: %zu Start stamp: %lu End stamp: %lu Total Samples: %u", 
+                start_stamps.size(), start_stamps[0], start_stamps[samples.size() - 1], samples.at(samples.size() - 1));
     if (has_magnetic_field_readings) {
       imu_extractor.getPayloadStamps(STR2FOURCC("MAGN"), start_stamps, samples);
-      ROS_INFO_STREAM("[MAGN] Payloads: " << start_stamps.size()
-                                          << " Start stamp: " << start_stamps[0]
-                                          << " End stamp: " << start_stamps[samples.size() - 1]
-                                          << " Total Samples: " << samples.at(samples.size() - 1));
+      RCLCPP_INFO(node->get_logger(), 
+                  "[MAGN] Payloads: %zu Start stamp: %lu End stamp: %lu Total Samples: %u", 
+                  start_stamps.size(), start_stamps[0], start_stamps[samples.size() - 1], samples.at(samples.size() - 1));
     }
 
     uint64_t accl_end_stamp = 0, gyro_end_stamp = 0;
@@ -137,31 +150,32 @@ int main(int argc, char* argv[]) {
     uint32_t gpmf_frame_count = imu_extractor.getImageCount();
     uint32_t ffmpeg_frame_count = video_extractor.getFrameCount();
     if (gpmf_frame_count != ffmpeg_frame_count) {
-      ROS_FATAL_STREAM("Video and metadata frame count do not match");
-      ros::shutdown();
+      RCLCPP_FATAL(rclcpp::get_logger("gopro_to_rosbag"),"Video and metadata frame count do not match");
+      rclcpp::shutdown();
     }
 
     uint64_t gpmf_video_time = imu_extractor.getVideoCreationTime();
     uint64_t ffmpeg_video_time = video_extractor.getVideoCreationTime();
 
     if (ffmpeg_video_time != gpmf_video_time) {
-      ROS_FATAL_STREAM("Video creation time does not match");
-      ros::shutdown();
+      RCLCPP_FATAL(rclcpp::get_logger("gopro_to_rosbag"), "Video creation time does not match");
+      rclcpp::shutdown();
     }
 
     imu_extractor.getImageStamps(image_stamps, video_end_stamp);
     if (i != video_files.size() - 1 && image_stamps.size() != ffmpeg_frame_count) {
-      ROS_FATAL_STREAM("ffmpeg and gpmf frame count does not match.");
-      ROS_FATAL_STREAM(image_stamps.size() << " vs " << ffmpeg_frame_count);
-      ros::shutdown();
+      RCLCPP_FATAL(rclcpp::get_logger("gopro_to_rosbag"), "ffmpeg and gpmf frame count does not match. %zu vs %u", 
+        image_stamps.size(), ffmpeg_frame_count);
+      rclcpp::shutdown();
     }
 
     video_extractor.writeVideo(
         bag, "/gopro/image_raw", image_stamps, grayscale, compress_images, display_images);
   }
 
-  ROS_INFO_STREAM("[ACCL] Payloads: " << accl_queue.size());
-  ROS_INFO_STREAM("[GYRO] Payloads: " << gyro_queue.size());
+  // write imu data
+  RCLCPP_INFO(node->get_logger(), "[ACCL] Payloads: %zu", accl_queue.size());
+  RCLCPP_INFO(node->get_logger(), "[GYRO] Payloads: %zu", gyro_queue.size());
 
   assert(accl_queue.size() == gyro_queue.size());
 
@@ -173,9 +187,10 @@ int main(int argc, char* argv[]) {
     int64_t diff = accl.timestamp_ - gyro.timestamp_;
 
     Timestamp stamp;
+
     if (abs(diff) > 100000) {
       // I will need to handle this case more carefully
-      ROS_WARN_STREAM(diff << " ns difference between gyro and accl");
+      RCLCPP_WARN(node->get_logger(), "%ld ns difference between gyro and accl", diff);
       stamp = (Timestamp)(((double)accl.timestamp_ + (double)gyro.timestamp_) / 2.0);
     } else {
       stamp = accl.timestamp_;
@@ -184,14 +199,13 @@ int main(int argc, char* argv[]) {
     double current = stamp * 1e-9;
     // assert(abs(current - previous) < 0.001);
     previous = current;
-    uint32_t secs = stamp * 1e-9;
-    uint32_t n_secs = stamp % 1000000000;
-    ros::Time ros_time(secs, n_secs);
-    sensor_msgs::Imu imu_msg;
-    std_msgs::Header header;
-    header.stamp = ros_time;
-    header.frame_id = "body";
-    imu_msg.header = header;
+
+    builtin_interfaces::msg::Time ros_time;
+    ros_time.sec = stamp / 1000000000;
+    ros_time.nanosec = stamp % 1000000000;
+    sensor_msgs::msg::Imu imu_msg;
+    imu_msg.header.stamp = ros_time;
+    imu_msg.header.frame_id = "body";
     imu_msg.linear_acceleration.x = accl.data_.x();
     imu_msg.linear_acceleration.y = accl.data_.y();
     imu_msg.linear_acceleration.z = accl.data_.z();
@@ -199,30 +213,31 @@ int main(int argc, char* argv[]) {
     imu_msg.angular_velocity.y = gyro.data_.y();
     imu_msg.angular_velocity.z = gyro.data_.z();
 
-    bag.write("/gopro/imu", ros_time, imu_msg);
+    bag.write("/gopro/imu", imu_msg, ros_time);
 
     accl_queue.pop_front();
     gyro_queue.pop_front();
   }
 
+  // write magnetometer data
   while (!magnetometer_queue.empty()) {
     MagMeasurement mag = magnetometer_queue.front();
     Timestamp stamp = mag.timestamp_;
 
-    uint32_t secs = stamp * 1e-9;
-    uint32_t n_secs = stamp % 1000000000;
-    ros::Time ros_time(secs, n_secs);
-    sensor_msgs::MagneticField magnetic_field_msg;
-    std_msgs::Header header;
-    header.stamp = ros_time;
-    header.frame_id = "body";
-    magnetic_field_msg.header = header;
+    builtin_interfaces::msg::Time ros_time;
+    ros_time.sec = stamp / 1000000000;
+    ros_time.nanosec = stamp % 1000000000;
+    sensor_msgs::msg::MagneticField magnetic_field_msg;
+    magnetic_field_msg.header.stamp = ros_time;
+    magnetic_field_msg.header.frame_id = "body";
     magnetic_field_msg.magnetic_field.x = mag.magfield_.x();
     magnetic_field_msg.magnetic_field.y = mag.magfield_.y();
     magnetic_field_msg.magnetic_field.z = mag.magfield_.z();
 
-    bag.write("/gopro/magnetic_field", ros_time, magnetic_field_msg);
+    bag.write("/gopro/magnetic_field", magnetic_field_msg, ros_time);
 
     magnetometer_queue.pop_front();
   }
+  rclcpp::shutdown();
+  return 0;
 }
